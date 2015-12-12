049390a02ef81e6b8d4736dc778e3296faffe5de
Diego Faria <diegofndefaria@gmail.com>
2015-12-12 21:33:39 -0200

    testing form posts

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express')
 var path = require('path')
+var bodyParser = require('body-parser')
 var port = process.argv[2] || 3000
 var app = express()
+
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.set('view engine', 'jade')
 app.set('views', path.join(__dirname, 'templates'))
@@ -11,6 +15,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(request, response) {
     response.render('index', {date: new Date().toDateString()})
 })
+
+app.post('/who', function(request, response) {
+    console.log(request.body.name)
+    response.send(request.body.name)
+})
+
 
 app.listen(port)
 console.log("Server is up and running.");
