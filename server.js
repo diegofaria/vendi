941378ed826b88df783fc01fb546d75fa5d67617
Diego Faria <diegofndefaria@gmail.com>
2015-12-12 19:54:44 -0200

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var path = require('path')
+var port = process.argv[2] || 3000
+var app = express()
+
+app.set('view engine', 'jade')
+app.set('views', path.join(__dirname, 'templates'))
+
+app.get('/', function(request, response) {
+    response.render('index', {date: new Date().toDateString()})
+})
+
+app.listen(port)
+console.log("Server is up and running.");
