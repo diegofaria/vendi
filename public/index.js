beeb0fcbc44a16a9f568302ada8e222c9eb3eec9
Diego Faria <diegofndefaria@gmail.com>
2015-12-16 00:19:48 -0200

    building interchangeable inputs

--- public/index.js
+++ public/index.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
-    $('#person-name').keydown(function(event){
+    $('#who').keydown(function(event){
+      if (event.keyCode == 13) {
+          event.preventDefault();
+          console.log("enter");
+          $('.js-howMuch-group').show();
+          $('.js-who-group').hide();
+      }
+    })
+
+    $('#how-much').keydown(function(event){
       if (event.keyCode == 13)
         event.preventDefault();
         console.log("enter");
     })
+
 })
