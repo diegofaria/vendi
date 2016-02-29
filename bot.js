4a4b203320301c9409f36dfae4fbf923ac06bb8e
Diego Faria <diegofndefaria@gmail.com>
2016-02-29 19:03:56 -0300

    added slackbots dependency

---
+++ bot.js
@@ -0,0 +1,18 @@
+var SlackBot = require('slackbots');
+
+// create a bot
+var bot = new SlackBot({
+    token: 'TOKE_HERE',
+    name: 'Natalie Portman'
+});
+
+bot.on('start', function() {
+    // define existing username instead of 'user_name'
+    bot.postMessageToUser('diegofaria', 'Olá!', function(data) {});
+    console.log(bot.users)
+});
+
+bot.on('message', function(data) {
+    // all ingoing events https://api.slack.com/rtm
+    console.log('user: ' + data.user)
+});
