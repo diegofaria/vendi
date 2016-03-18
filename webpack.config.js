0080dc6112d1513cf00a680388a9808d41a51819
Diego Faria <diegofndefaria@gmail.com>
2016-03-18 00:27:02 -0300

    added webpack support

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './public/index',
+  output: {
+    path: __dirname + '/public/dist',
+    filename: 'index-bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+    ]
+  }
+};
