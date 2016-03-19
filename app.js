f4092266653e903b91d888ef43056092694216e5
Diego Faria <diegofndefaria@gmail.com>
2016-03-19 19:05:59 -0300

    using express.Router

--- app.js
+++ app.js
@@ -4,52 +4,12 @@ var path = require('path')
 var bodyParser = require('body-parser')
 var app = express()
 
-var ListCustomersBalances = require('./core/usecases/list_customers_balances')
-var SaveTransaction = require('./core/usecases/save_transaction')
-var TransactionGateway = require('./core/gateways/transaction_gateway')
-var gateway = new TransactionGateway()
-
 app.set('port', (process.env.PORT || 3000));
-app.use(morgan('common'))
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json());
+app.use(morgan('dev'))
 app.use('/', express.static(path.join(__dirname, 'public')));
-// Additional middleware which will set headers that we need on each request.
-app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
-    res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Disable caching so we'll always get the latest comments.
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
-});
-
-app.post('/api/transactions', function(req, res) {
-    var newTransaction = {
-        who: req.body.who,
-        howMany: req.body.howMany,
-        howMuch: req.body.howMuch,
-    };
-    new SaveTransaction(gateway, {
-        error: function(){response.send("explosion")},
-        success: function(){
-            new ListCustomersBalances(gateway, {
-                list: function(balances){
-                    res.json(balances)
-                }
-            }).execute()
-        }
-    }).execute(newTransaction)
-});
-
-app.get('/api/transactions', function(req, res) {
-    new ListCustomersBalances(gateway, {
-        list: function(balances){
-            res.json(balances)
-        }
-    }).execute()
-});
+var transactions = require('./transactions')
+app.use('/api/transactions', transactions)
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
---
+++ transactions.js
@@ -0,0 +1,43 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var ListCustomersBalances = require('./core/usecases/list_customers_balances')
+var SaveTransaction = require('./core/usecases/save_transaction')
+var TransactionGateway = require('./core/gateways/transaction_gateway')
+var gateway = new TransactionGateway()
+
+var transactionsRouter = express.Router()
+transactionsRouter.use(function(req, res,next){
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Cache-Control', 'no-cache')
+    next()
+})
+transactionsRouter.use(bodyParser.json())
+transactionsRouter.use(bodyParser.urlencoded({extended: false}))
+transactionsRouter.route('/')
+.post(function(req, res) {
+    var newTransaction = {
+        who: req.body.who,
+        howMany: req.body.howMany,
+        howMuch: req.body.howMuch,
+    }
+    new SaveTransaction(gateway, {
+        error: function(){response.send("explosion")},
+        success: function(){
+            new ListCustomersBalances(gateway, {
+                list: function(balances){
+                    res.json(balances)
+                }
+            }).execute()
+        }
+    }).execute(newTransaction)
+})
+.get(function(req, res) {
+    new ListCustomersBalances(gateway, {
+        list: function(balances){
+            res.json(balances)
+        }
+    }).execute()
+});
+
+module.exports = transactionsRouter
