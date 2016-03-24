6fa4572661e019e4fa5c8db34bac02c7ba6859df
Diego Faria <diegofndefaria@gmail.com>
2016-03-23 22:40:00 -0300

    servin static to /, moved html

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ var app = express()
 
 app.set('port', (process.env.PORT || 3000));
 app.use(morgan('dev'))
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.sendfile(path.join(__dirname, '/views/index.html'));
+});
 
 var transactions = require('./routes/transactions')
 app.use('/api/transactions', transactions)
